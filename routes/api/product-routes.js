665a5226cbdc956883cc49652d451cc5d2033079
Fatemeh Niaziorimi <fatemehniaziorimi@macbook-pro.mynetworksettings.com>
2023-02-20 21:17:12 -0500

    adding gif and video links to READMEfile

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -45,7 +45,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -77,7 +77,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -118,8 +118,26 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+
+try{
+  const destroyProduct = await Product.destroy({
+    where: {
+      id:req.params.id
+    }
+  });
+  if(!destroyProduct){
+    return req.status(400).json({
+      messgae: "Product not found"
+    });
+  }
+  return res.status(200).json(destroyProduct);
+}
+catch(err){
+  res.status(500).json(err);
+}
+
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,11 +53,11 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 try{
   const updateTag = await Tag.update({
-    Tag_name: req.body.tag_name
+    tag_name: req.body.tag_name
   }, {
     where: {
         id:req.params.id
@@ -75,7 +75,7 @@ try{
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try{
     const destroyTag = await Tag.destroy({
